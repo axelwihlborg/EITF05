2c2297d51069d4ae0354c950ae70eac26e279031
CarlTern <35721844+CarlTern@users.noreply.github.com>
2018-10-03 10:50:12 +0200

    Add files via upload

---
+++ hemsidatest2/merge this.sql
@@ -0,0 +1,105 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 03 okt 2018 kl 10:48
+-- Serverversion: 10.1.35-MariaDB
+-- PHP-version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `user_registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('Carl', 'Ca12345'),
+('carlte', 'Ca12345'),
+('carltest', 'Carltest1'),
+('cc', 'ccccc123C'),
+('farm', 'Farm123'),
+('farm123', 'farm123CC'),
+('hej', 'Hejhej1');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `tblproduct`
+--
+
+CREATE TABLE `tblproduct` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `tblproduct`
+--
+
+INSERT INTO `tblproduct` (`id`, `name`, `code`, `image`, `price`) VALUES
+(1, 'Table', 'Table', 'product-images/table.jpg', 1500.00),
+(2, 'Chair', 'Chair', 'product-images/chair.jpg', 800.00),
+(3, 'Armchair', 'Armchair', 'product-images/armchair.jpg', 300.00),
+(4, 'Desk', 'Desk', 'product-images/desk.jpg', 800.00),
+(6, 'Salas', 'Salas', 'product-images/salas.jpg', 8000.00);
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+ALTER TABLE `login` ADD FULLTEXT KEY `username` (`username`,`password`);
+
+--
+-- Index för tabell `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`code`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
