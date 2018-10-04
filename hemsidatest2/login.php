4150da3dfe705941d90feadbbad49889a23a8435
CarlTern <35721844+CarlTern@users.noreply.github.com>
2018-10-04 11:12:20 +0200

    Update login.php

--- hemsidatest2/login.php
+++ hemsidatest2/login.php
@@ -41,21 +41,23 @@ if(isset($_POST["submit"])){
 if(!empty($_POST['user']) && !empty($_POST['pass'])) {  
     $user=$_POST['user'];  
     $pass=$_POST['pass'];  
+	
   
     $con=mysqli_connect('localhost','root','') or die(mysqli_error());  
     mysqli_select_db($con,'user_registration') or die("cannot select DB");  
   
-    $query=mysqli_query($con,"SELECT * FROM login WHERE username='".$user."' AND password='".$pass."'");  
-    $numrows=mysqli_num_rows($query);  
+    $query=mysqli_query($con,"SELECT username, password FROM login WHERE username='".$user."'");  
+    
+	$numrows=mysqli_num_rows($query);  
     if($numrows!=0)  
     {  
     while($row=mysqli_fetch_assoc($query))  
     {  
     $dbusername=$row['username'];  
-    $dbpassword=$row['password'];  
+    $dbpassword=$row['password']; 
     }  
-  
-    if($user == $dbusername && $pass == $dbpassword)  
+
+    if($user == $dbusername && password_verify($pass, $dbpassword))  
     {  
     session_start();  
     $_SESSION['sess_user']=$user;  
